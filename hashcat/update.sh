3c9f4742232f34a30dd02111290cfdc0a79bf157
Jon <krypt3dkn1gh7@gmail.com>
2017-04-26 16:18:56 -0400

    Create update.sh

---
+++ hashcat/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CHECKING=`apt update 2>/dev/null`
+
+function logme() {
+	for account in me you and mom; do
+		printf "`date`: $1\n" >> /home/$account/HashcatLogging.txt
+	done
+}
+
+if [[ $CHECKING != *"can be upgraded"* ]] ; then
+	MESSAGE=`printf "No updates are available\n"`
+	logme "$MESSAGE"
+else
+	MESSAGE=`printf "$CHECKING" | grep "can be upgraded" | cut -d "." -f 1 | sed "s/can be/were/"`
+	apt upgrade -y 2>/dev/null || logme "Uh-oh! Something went wrong upgrading!"
+	apt dist-upgrade -y 2>/dev/null || logme "Uh-oh! Something went wrong with dist-upgrade!"
+	apt autoremove -y 2>/dev/null || logme "Uh-oh! Something went wrong with autoremoving!"
+	logme "$MESSAGE"
+fi
